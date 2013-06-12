131c35a842d17f12b8b81d41b581017b051a7e3c
noizeR3 <noizethehacker@hotmail.com>
2013-06-12 20:33:40 +0300

    Update and rename say.php to goto.php
    Some changes in comments syntax; added a command example at the bof.

---
+++ interpreter/keywords/goto.php
@@ -0,0 +1,20 @@
+<?php
+
+// goto("http://google.com")
+
+class goto implements keywords{
+	private $error = NULL;		// we will store errors here
+
+	public function error(){	// this will be called when some error is found
+		return $this->error;
+	}
+
+	public function syntax($string){
+		// when no arguments are specified window location doesn't change
+		return true;
+	}
+	public function run($string){	// we are redirecting the document's location to the new (if there's any) location specified anything found after the command
+		header("Location:" . $string);
+	}
+} 
+?>
